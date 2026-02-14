989c3d7495fad30ea4fef5cfbb68d7505c0e53b1
Adrián Juárez <comecacahuates@yahoo.com>
2026-02-14 17:57:40 -0600

    fix(lambda/go): add missing main.tf file

--- lambda/go/main.tf
+++ lambda/go/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 6.0"
-    }
-  }
-}
-
 module "lambda" {
   source = "../standard"
 
@@ -16,11 +6,9 @@ module "lambda" {
   runtime       = "provided.al2023"
   source_file   = var.source_file
 
-  memory_size                    = var.memory_size
-  timeout                        = var.timeout
-  log_retention_days             = var.log_retention_days
-  reserved_concurrent_executions = var.reserved_concurrent_executions
-  architectures                  = var.architectures
+  memory_size        = var.memory_size
+  timeout            = var.timeout
+  log_retention_days = var.log_retention_days
 
   environment_variables = var.environment_variables
   policy_statements     = var.policy_statements
